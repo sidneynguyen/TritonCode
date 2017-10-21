9e979361227d382db8cdd1ff246b0c88bee19ad1
Sidney Nguyen <sidneynguyen@Sidneys-MacBook-Pro.local>
2017-10-21 00:52:27 -0700

    WebSocket example

--- TritonCodeServer/src/main/java/TritonCodeServer.java
+++ TritonCodeServer/src/main/java/TritonCodeServer.java
@@ -1,10 +1,40 @@
-import static spark.Spark.get;
-import static spark.Spark.port;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
+import static spark.Spark.*;
+
+@WebSocket
 public class TritonCodeServer {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
         port(3000);
-        get("/", (req, res) -> "Hello World");
+        staticFileLocation("/public");
+        webSocket("/chat", TritonCodeServer.class);
+        init();
+    }
+
+    private String sender, msg;
+
+    @OnWebSocketConnect
+    public void onConnect(Session user) throws Exception {
+        String username = "User" + Chat.nextUserNumber++;
+        Chat.userUsernameMap.put(user, username);
+        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session user, int statusCode, String reason) {
+        String username = Chat.userUsernameMap.get(user);
+        Chat.userUsernameMap.remove(user);
+        Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session user, String message) {
+        Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
     }
 }
