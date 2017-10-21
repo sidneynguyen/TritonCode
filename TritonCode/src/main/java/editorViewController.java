a65f0f5b0a399b320cf4f5265b99ca02fc547c9b
Jason <jug016@ucsd.edu>
2017-10-21 01:03:12 -0700

    Finished basic layout

--- TritonCode/src/main/java/editorViewController.java
+++ TritonCode/src/main/java/editorViewController.java
@@ -1,12 +1,32 @@
 import  javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class editorViewController extends Application{
+    @FXML
+    private Button newButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private TextArea editor;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+
         launch(args );
     }
 
@@ -14,10 +34,58 @@ public class editorViewController extends Application{
     public void start (Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("textEditor.fxml"));
+
+        newButton = new Button("New File");
+
+        newButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                System.out.println("New FIle");
+            }
+        });
+
+        saveButton = new Button("Save");
+        saveButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                System.out.println("save");
+            }
+        });
+        HBox buttonLayout = new HBox();
+        buttonLayout.getChildren().addAll(newButton,saveButton);
+        BorderPane layout = new BorderPane();
+
+        editor = new TextArea();
+        editor.setPrefSize(layout.getWidth(), layout.getHeight());
+        editor.setWrapText(true);
+
+
+        layout.setTop(buttonLayout);
+        layout.setCenter(editor);
+
+
+        Scene scene = new Scene(layout, 600, 400);
+        layout.setPrefHeight(scene.getHeight());
+        layout.setPrefWidth(scene.getWidth());
+
+
+
         primaryStage.setTitle("Triton Code");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(scene);
+
+
+
         primaryStage.show();
 
 
     }
+
+    @FXML
+    protected void handleNewButtonAction (ActionEvent event){
+
+        System.out.println("New Button clicked");
+
+    }
+
+
 }
